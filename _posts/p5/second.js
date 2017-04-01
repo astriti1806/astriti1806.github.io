235f5744f3d8125962d723d7be2311bcc215d4e5
astriti1806 <astriti1806@gmail.com>
2017-04-01 14:24:39 +0530

    Create second.js

---
+++ _posts/p5/second.js
@@ -0,0 +1,20 @@
+function setup(){
+	
+createCanvas(400,400);
+}
+var x=100
+var count=0
+function draw(){
+	background(220);
+  clear();
+	arc(x,200, 100, 100,7,5.6, PIE);
+	if(count>=0);  
+	ellipse(200,200,15, 15);
+  if(count>=0);
+	ellipse(250,200,15,15);
+  if(count>=0);
+	ellipse(300,200,15,15);
+  if(count>=0);
+	ellipse(350,200,15,15);
+  x=x+1
+}
